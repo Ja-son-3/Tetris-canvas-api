c4fe1ea579627b6e039951ebfe87e5a0594568f7
Jason Huang <jason97521@gmail.com>
2023-02-20 11:06:34 -0500

    Fixed Errors

--- board.js
+++ board.js
@@ -1,12 +1,11 @@
 class Board {
-
-    ctx
-    ctxNext
-    grid
-    piece
-    next
-    requestId
-    time
+    ctx;
+    ctxNext;
+    grid;
+    piece;
+    next;
+    requestId;
+    time;
 
     constructor(ctx, ctxNext) {
         this.ctx = ctx
@@ -15,8 +14,8 @@ class Board {
     }
 
     init() {
-        ctx.canvas.width = COLS * BLOCK_SIZE
-        ctx.canvas.height = ROWS * BLOCK_SIZE
+        ctx.canvas.width = COLS * BLOCK_SIZE;
+        ctx.canvas.height = ROWS * BLOCK_SIZE;
 
         ctx.scale(BLOCK_SIZE,BLOCK_SIZE)
     }
@@ -30,7 +29,9 @@ class Board {
 
     getNewPiece() {
         this.next = new Piece(this.ctxNext)
-        this.ctxNext.clearRect(0,0,this.ctxNext.canvas.width,this.ctxNext.canvas.height)
+        this.ctxNext.clearRect(0,0,
+            this.ctxNext.canvas.width,
+            this.ctxNext.canvas.height);
         this.next.draw()
     }
 
@@ -47,6 +48,7 @@ class Board {
             this.freeze()
             this.clearLines()
             if(this.piece.y === 0) {
+                // Game over
                 return false
             }
             this.piece = this.next
@@ -57,26 +59,48 @@ class Board {
         return true
     }
 
-    clearLine () {
+    clearLines () {
         let lines = 0
         this.grid.forEach((row,y) => {
-            if(row.every(value => value > 0)) {
+            if(row.every(value => value >0)) {
                 lines++
-                this.grid.splice(y, 1)
+                this.grid.splice(y,1)
                 this.grid.unshift(Array(COLS).fill(0))
             }
         })
 
-        if (lines > 0) {
+        if (lines>0) {
             account.score += this.getLinesClearedPoints(lines)
             account.lines += lines
-            
+
             if(account.lines >= LINES_PER_LEVEL) {
                 account.level++
                 account.lines -= LINES_PER_LEVEL
                 time.level = LEVEL[account.level]
             }
         }
+    }
+
+    freeze() {
+        this.piece.shape.forEach((row,y) => {
+            row.forEach((value,x)  => {
+                if(value > 0) {
+                    this.grid[y + this.piece.y][x + this.piece.x] = value
+                }
+            })
+        }
+        )
+    }
+
+    drawBoard() {
+        this.grid.forEach((row,y) => {
+            row.forEach((value, x) => {
+                if(value > 0) {
+                    this.ctx.fillStyle = COLORS[value]
+                    this.ctx.fillRect(x,y,1,1)
+                }
+            })
+        })
     }
 
     getEmptyGrid() {
@@ -86,12 +110,12 @@ class Board {
     }
 
     valid(p) {
-        return p.shape.every((row,dy) => {
+        return p.shape.every((row,dy)=> {
             return row.every((value,dx) => {
                 let x = p.x + dx
                 let y = p.y + dy
                 return (
-                    value === 0 || 
+                    value === 0 ||
                     (this.insideWalls(x) && this.aboveFloor(y) && this.notOccupied(x,y))
                 )
             })
@@ -99,7 +123,7 @@ class Board {
     }
 
     insideWalls(x) {
-        return x >= 0 && x < COLS 
+        return x >= 0 && x< COLS
     }
 
     aboveFloor(y) {
@@ -107,20 +131,35 @@ class Board {
     }
 
     notOccupied(x,y) {
-        return this.grid[y] && this.grid[y][x] === 0
+        return this.grid[y] && this.grid[y][x] ===0
     }
 
     rotate(p) {
-        let clone = JSON.parse(JSON.stringify(p))
+        let clone = JSON.parse(JSON.stringify(p));
         for (let y = 0; y < p.shape.length; ++y) {
             for (let x = 0; x < y; ++x) {
-                [p.shape[x][y], p.shape[y][x]] =
-                [p.shape[y][x], p.shape[x][y]]
+              [p.shape[x][y], p.shape[y][x]] = 
+              [p.shape[y][x], p.shape[x][y]];
             }
-        }
+          }
+          
+          // Reverse the order of the columns.
+          p.shape.forEach(row => row.reverse());
+          return p;
+    }
 
-        p.shape.forEach(row => row.reverse())
-        return p
+    getLinesClearedPoints(lines,level) {
+        const lineClearPoints = 
+            lines ===1
+                ? POINTS.SINGLE
+                : lines === 2
+                ? POINTS.DOUBLE
+                : lines === 3
+                ? POINTS.TRIPLE
+                : lines === 4
+                ? POINTS.TETRIS
+                : 0
+
+        return (account.level + 1) * lineClearPoints
     }
 }
-
